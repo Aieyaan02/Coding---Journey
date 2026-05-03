04a50c9af39a7bad4c1f995165e23760877a9f53
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-03 05:43:13 -0400

    Inverted Pyramid

--- halfPyramid.java
+++ halfPyramid.java
@@ -1,9 +1,9 @@
 public class halfPyramid {
     public static void main (String [] args){
-        int n = 4;
+        int row = 4;
 
-        for(int i = 1; i <= n; i++){
-            for (int j = 1; j<= i;j++){
+        for(int counter = 1; counter <= row; counter ++){
+            for (int j = 1; j<= counter;j++){
                 System.out.print("*");
             }
             System.out.println();
