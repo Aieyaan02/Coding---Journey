5361f68d6fa502212d0d555ead2fa4ee40dc8ede
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-05 20:55:06 -0400

    Practice

--- InvertPyra180.java
+++ InvertPyra180.java
@@ -1,11 +1,13 @@
 public class InvertPyra180 {
-    public static void main (String []args ){
-        int n = 4;
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n-i; j++){
+    public static void main(String []args){
+        int row = 4;
+        //outer loop
+        for(int i = 1; i <= row; i++){
+            //inner loop -> space print
+            for(int j = 1; j <= row-i; j++){
                 System.out.print(" ");
             }
+            //inner loop -> star print
             for (int j =1; j<= i; j++){
                 System.out.print("*");
             }
---
+++ PracticeInvertPyra.java
@@ -0,0 +1,15 @@
+public class PracticeInvertPyra {
+    public static void main (String []args){
+        int row = 4;
+        for (int i = 1; i<= row; i++){
+            for (int j = 1; j<= row - i;j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j<= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
