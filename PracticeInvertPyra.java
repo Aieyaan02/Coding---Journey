0d59e68b3f298f34106c1a5a220357a972231c04
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-06 18:11:36 -0400

    Pattern print

--- PracticeInvertPyra.java
+++ PracticeInvertPyra.java
@@ -1,15 +1,16 @@
 public class PracticeInvertPyra {
     public static void main (String []args){
-        int row = 4;
-        for (int i = 1; i<= row; i++){
-            for (int j = 1; j<= row - i;j++){
+        int row = 5;
+
+        for(int counter = 1; counter <= row; counter++){
+            for (int j = 1; j<= row -1; j++){
                 System.out.print(" ");
             }
-            for (int j = 1; j<= i; j++){
-                System.out.print("*");
-            }
             System.out.println();
-
+            for(int printStar = 1; printStar >= counter;printStar++)
+            System.out.print("*");
         }
+            System.out.println();
     }
 }
+    
