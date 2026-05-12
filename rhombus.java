16c7ca7fff2cfb544bdbbe596ff4861b57aa3696
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-12 18:36:03 -0400

    Number Pyramid

--- rhombus.java
+++ rhombus.java
@@ -1,15 +1,18 @@
 public class rhombus {
     public static void main (String []args){
-        int n = 5;
+        int n = 8;
+        //outer loops
         for (int i = 1; i <= n; i++){
-            for (int j = 1; j<= n-1; j++){
+            // inner loops
+            for (int j = 1; j <= n-i; j++){
                 System.out.print(" ");
             }
-            for(int x = 1; x <= n-1;x++){
-                System.out.print("* * * * *");
+            //stars
+            for(int x = 1; x <= n; x++){
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
     
-}
+} 
