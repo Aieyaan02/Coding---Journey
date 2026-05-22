f6110aa467ba92531a84d22d5378c925bcae749a
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-22 00:11:16 -0400

    More practice

--- CheckEven.java
+++ CheckEven.java
@@ -1,6 +1,24 @@
-public class CheckEven {
-    public static void main(String []args){
-        
+import java.util.Scanner;
+    public class CheckEven {
+    
+    public static int check_even (int n){
+        if (n%2 == 0){
+            System.out.println("even");
+        } else {
+            System.out.println("Odd");
+        }
+        return n;
+    }
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your number : ");
+        int num = sc.nextInt();
+        sc.close();
+        System.out.println("Your number is "+ check_even(num));
+
+
     }
     
 }
+    
+
