2c6cfd418a3721e5b4caa10ced56449ec3e9a6dd
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-29 03:38:07 -0400

    Practice

--- TableGiven_n.java
+++ TableGiven_n.java
@@ -1,12 +1,10 @@
+import java.util.Scanner;
 public class TableGiven_n {
     public static void main (String[]args){
-        int a  = 3;
-        int b = 10;
-
-        int sum = a + b;
-
-        System.out.println("Do the sum: " + sum);
-
+        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
 
 
 
