d53e9077b010e5f286c82943e8a87392acff7ddc
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-28 22:28:45 -0400

    working

--- TableGiven_n.java
+++ TableGiven_n.java
@@ -1,6 +1,14 @@
 public class TableGiven_n {
     public static void main (String[]args){
-        
+        int a  = 3;
+        int b = 10;
+
+        int sum = a + b;
+
+        System.out.println("Do the sum: " + sum);
+
+
+
 
     }
     
