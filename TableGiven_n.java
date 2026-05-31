cdbd45a085f0e7e628bd4ea1b6c946bea7e7f0b3
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-05-31 18:49:17 -0400

    Practice

--- TableGiven_n.java
+++ TableGiven_n.java
@@ -1,13 +1,6 @@
 import java.util.Scanner;
 public class TableGiven_n {
     public static void main (String[]args){
-        
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int sum = 2 + n;
-        System.out.println(sum);
-        scan.close();
-
 
     }
     
