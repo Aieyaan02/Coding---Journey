23803e13002c825fe8ef6490d9ecf6d541019d60
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-02 22:13:38 -0400

    practice

--- TableGiven_n.java
+++ TableGiven_n.java
@@ -1,11 +1,21 @@
-//import java.util.Scanner;
+import java.util.Scanner;
+
 public class TableGiven_n {
+    public static int table (int a){
+        
+        for(int i = 1; i <= 10;i++){
+            System.out.println(a*i);
 
-
-
-    
+        }
+        return a;
+    }
     public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = scan.nextInt();
+        System.out.println("Given number is : "+ table(num));
 
+        scan.close();
     }
     
 }
--- checkPrime.java
+++ checkPrime.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 public class checkPrime {
 
-    public static int prime (int n){
+    public static int isPrime (int n){
         // prime number is a number that can only be divided but 1 and the number itself.
-        if(n<=1){
+        if(n <= 1){
             System.out.println("Invalid number");
-        } else {
-            for(int i=2;i<n;i++){
-                if(n%i==0){
-                    System.out.println("Not Prime");
-                } else {
-                    System.out.println("Prime");
-                }               
-
-            }
-
         }
-        return n;
+        for(int i = 2;i <= n/2;i++){
+            if(n%i == 0){
+                System.out.println("Not a prime number");
+            } else {
+                System.out.println("Is a prime number");
+            }
+        
+        }
+       return n;
     }
 
     public static void main (String []args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter an number: ");
         int n = scan.nextInt();
-        prime(n);
+        System.out.println("Given number is : "+ isPrime(n));
         scan.close();
+
     }
 
 }
