76753c12b63a0f15fd594386c5be260230d84a70
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-03 22:22:06 -0400

    work

--- checkPrime.java
+++ checkPrime.java
@@ -3,26 +3,11 @@ public class checkPrime {
 
     public static int isPrime (int n){
         // prime number is a number that can only be divided but 1 and the number itself.
-        if(n <= 1){
-            System.out.println("Invalid number");
-        }
-        for(int i = 2;i <= n/2;i++){
-            if(n%i == 0){
-                System.out.println("Not a prime number");
-            } else {
-                System.out.println("Is a prime number");
-            }
-        
-        }
-       return n;
+        return n;
     }
 
     public static void main (String []args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter an number: ");
-        int n = scan.nextInt();
-        System.out.println("Given number is : "+ isPrime(n));
-        scan.close();
+        
 
     }
 
