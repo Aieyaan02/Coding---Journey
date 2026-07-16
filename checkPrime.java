61463fecbe64e112b82709ec5d85c3d337a82c1a
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-07-16 04:07:56 -0400

    S

--- checkPrime.java
+++ checkPrime.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 public class checkPrime {
 
     public static int isPrime (int n){
-        // prime number is a number that can only be divided but 1 and the number itself.
-        
+        // prime number is a number that can only be divided but 1 and the number itself.       
             return n;                 
     }
 
