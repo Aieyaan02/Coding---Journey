108d7d5cc81b945d491057f9662138ed7bbbcde1
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-07-20 21:02:19 -0400

    S

--- checkPrime.java
+++ checkPrime.java
@@ -5,6 +5,9 @@ public class checkPrime {
         // prime number is a number that can only be divided but 1 and the number itself.
            
             return n;                 
+
+
+            
     }
 
     public static void main (String []args){
