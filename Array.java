183b0b502eefd180360a4903b7b543f67011a029
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-07-24 00:11:26 -0400

    S

--- Array.java
+++ Array.java
@@ -1,6 +1,6 @@
 public class Array {
     public static void main (String[]args){
-        int marks[] = {98,95,92};
+        int marks[ ] = {98,95,92};
        // int[] marks = new int[3];
        // marks[0] = 98;
        // marks[1] = 95;
