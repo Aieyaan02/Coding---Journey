e704db96031512724701e7d54a5d61706f1933d6
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-07-29 22:24:18 -0400

    S

--- InvertPyra180.java
+++ InvertPyra180.java
@@ -5,7 +5,7 @@ public class InvertPyra180 {
         for(int i = 1; i <= row; i++){
             //inner loop -> space print
             for(int j = 1; j <= row-i; j++){
-                System.out.print(" ");
+                System.out.print("    ");
             }
             //inner loop -> star print
             for (int j =1; j<= i; j++){
