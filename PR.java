69505f0d4354230d946a96630bddfe0f1153c37d
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-07-30 03:57:22 -0400

    S

--- PR.java
+++ PR.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 public class PR {
     public static void main (String[]args){
-
+        average(2,5,6);
 
     }
-    public static void average (int n){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int a = scan.nextInt();
-        System.out.println("Enter second number: ");
-        int b = scan.nextInt();
-        System.out.println("Enter third number: ");
-        int c = scan.nextInt();
-        scan.close();       
-        int average = (a + b + c)/3;
-        System.out.println(average);
+    public static void average (int a, int b, int c){      
+        int math = (a + b + c)/3;
+        System.out.println(math);
     }
     
 }
