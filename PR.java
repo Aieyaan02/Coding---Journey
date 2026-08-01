522c1961e3067708e97147475207a2ba79cc2849
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-08-01 04:20:19 -0400

    S

--- PR.java
+++ PR.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
     public static void main (String []args){
-
+        Scanner scan = new Scanner (System.in);
         
+
+
         
     }
     static Double circumference (Double radius){
