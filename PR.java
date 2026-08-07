5aebbe4e949a45210c524068e85964bc5c3a9169
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-08-07 03:47:21 -0400

    :S:

--- PR.java
+++ PR.java
@@ -3,11 +3,7 @@ import java.util.Scanner;
     }
     static void infiniteloop(){   
         System.out.println();
-        Scanner scan = new Scanner(System.in);
-
-
-
-        
+        Scanner scan = new Scanner(System.in);       
         scan.close();   
     }
 /*/ public static void main (String[]args){
