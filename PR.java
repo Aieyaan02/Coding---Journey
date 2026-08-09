d3e940eb574ab375cddce6f332a190bdf14a25f0
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-08-09 15:22:50 -0400

    S

--- PR.java
+++ PR.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
         System.out.println();
         Scanner scan = new Scanner(System.in);       
         scan.close();   
+        System.out.println();
+        sun.instrument
     }
 /*/ public static void main (String[]args){
     age();
