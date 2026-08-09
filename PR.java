87528eb1ba3db6b46b505d20a656cf6b1478bce7
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-08-09 15:25:26 -0400

    S

--- PR.java
+++ PR.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
         
     }
 /*/ public static void main (String[]args){
