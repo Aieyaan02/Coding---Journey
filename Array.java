575882b78b5ace1882eab07a824e0ac5d8cff4df
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-08-21 00:04:02 -0400

    S

--- Array.java
+++ Array.java
@@ -3,22 +3,16 @@ public class Array {
     public static void main (String[]args){
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
-        int number[] = new int[size];
-        //intput
-        for (int i = 0;i<size;i++){
-            number[i]= scan.nextInt();
+        String[]name = new String[size];
+
+        for(int i = 0; i < size;i++){
+            name[i] = scan.next();
         }
-        int x = scan.nextInt();
-        //output
-        for (int i = 0; i<number.length;i++){
-            if (number[i]==x){
-                System.out.println("x found at " + i);
-            }
+        for(int i = 0; i < size;i++){
+            System.out.println(name[i]);
+
+        }
+        
         }
     } 
-}
 
-static void names(){
-    Scanner scan = new Scanner(System.in);
-    System.out.println();
-}
