bf349f13a098d724c1f8f44b937940e89af608ce
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-08-22 04:41:40 -0400

    S

--- Array.java
+++ Array.java
@@ -12,7 +12,6 @@ public class Array {
             System.out.println(); 
             System.out.println();
             System.out.println();
-            System.out.println();
         }
         for(int i = 0; i < size;i++){
             System.out.println(name[i]);
