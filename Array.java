c3f547ee645065ba7a953c8318ee04f18c0c4745
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-08-25 01:39:24 -0400

    S

--- Array.java
+++ Array.java
@@ -8,6 +8,7 @@ public class Array {
         for(int i = 0; i < size;i++){
             name[i] = scan.next();
             scan.close();  
+            System.out.println();
         }
         for(int i = 0; i < size;i++){
             System.out.println(name[i]);
