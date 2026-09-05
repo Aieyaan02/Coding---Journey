d1823814cee0ac7680a2b8f96e5f0e3073da8cbb
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-09-05 04:41:45 -0400

    S

--- Array.java
+++ Array.java
@@ -47,5 +47,8 @@ public class Array {
         System.out.println("Maximum value: " + max);
         // Fix 3: Close scanner outside the loop
         scan.close(); 
+        System.out.println(
+            
+        );
     } 
 }
